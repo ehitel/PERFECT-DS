1dd61aba04ddb933fad658a0f536557d324587d0
ehitel <ehitelrc@hotmail.com>
2015-10-08 07:12:50 -0600

    modified:   DS/DS/maestroPresentaciones.Designer.cs

--- DS/DS/maestroPresentaciones.Designer.cs
+++ DS/DS/maestroPresentaciones.Designer.cs
@@ -28,30 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Infragistics.Win.Appearance appearance2 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance1 = new Infragistics.Win.Appearance();
             this.ultraPanel1 = new Infragistics.Win.Misc.UltraPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ultraPanel1.ClientArea.SuspendLayout();
             this.ultraPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ultraPanel1
             // 
-            appearance2.BackColor = System.Drawing.Color.DimGray;
-            this.ultraPanel1.Appearance = appearance2;
+            appearance1.BackColor = System.Drawing.Color.DimGray;
+            this.ultraPanel1.Appearance = appearance1;
+            // 
+            // ultraPanel1.ClientArea
+            // 
+            this.ultraPanel1.ClientArea.Controls.Add(this.label2);
+            this.ultraPanel1.ClientArea.Controls.Add(this.label1);
             this.ultraPanel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.ultraPanel1.Location = new System.Drawing.Point(0, 0);
             this.ultraPanel1.Name = "ultraPanel1";
-            this.ultraPanel1.Size = new System.Drawing.Size(478, 66);
+            this.ultraPanel1.Size = new System.Drawing.Size(597, 63);
             this.ultraPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(4, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Maestro de presentaciones";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(6, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(297, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Información sobre los empaques de los artículos";
             // 
             // maestroPresentaciones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(478, 390);
+            this.ClientSize = new System.Drawing.Size(597, 485);
             this.Controls.Add(this.ultraPanel1);
             this.Name = "maestroPresentaciones";
             this.Text = "Presentaciones (empaques)";
+            this.ultraPanel1.ClientArea.ResumeLayout(false);
+            this.ultraPanel1.ClientArea.PerformLayout();
             this.ultraPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -60,5 +92,7 @@
         #endregion
 
         private Infragistics.Win.Misc.UltraPanel ultraPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
